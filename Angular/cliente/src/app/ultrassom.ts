f7c2c4603251852e14751c831dc16f8132ee574c
Matheus <matheuscruz@alunos.utfpr.edu.br>
2019-10-25 20:21:21 +0000

    Update ultrassom.ts

--- Angular/cliente/src/app/ultrassom.ts
+++ Angular/cliente/src/app/ultrassom.ts
@@ -1,8 +1,11 @@
 export class Ultrassom {
+    public idImagem: bigInteger;
     public idUsuario: BigInteger;
     public idAlgoritmo: BigInteger;
     public dataInicio: Date;
     public dataTermino: Date;
-    public tamanhoPixels: BigInteger;
+    public tamAltura: BigInteger;
+    public tamLargura: BigInteger;
     public iteracoes: BigInteger;
+    public imagem: string;
 }
