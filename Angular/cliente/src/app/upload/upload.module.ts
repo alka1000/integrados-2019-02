50ff0cfe38aee29b6d2fab37303c210f841fe92a
Matheus <matheuscruz@alunos.utfpr.edu.br>
2019-11-08 03:23:10 +0000

    Update upload.module.ts

--- Angular/cliente/src/app/upload/upload.module.ts
+++ Angular/cliente/src/app/upload/upload.module.ts
@@ -4,12 +4,11 @@ import { UploadComponent } from './upload.component';
 import { MatButtonModule, MatDialogModule, MatListModule, MatProgressBarModule } from '@angular/material';
 import { DialogComponent } from './dialog/dialog.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { UploadService } from './upload.service';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports: [CommonModule, MatButtonModule, MatDialogModule, MatListModule, FlexLayoutModule, HttpClientModule, BrowserAnimationsModule, MatProgressBarModule],
+  imports: [CommonModule, MatButtonModule, MatDialogModule, MatListModule, HttpClientModule, BrowserAnimationsModule, MatProgressBarModule],
   declarations: [UploadComponent, DialogComponent],
   exports: [UploadComponent],
   entryComponents: [DialogComponent], // Add the DialogComponent as entry component
